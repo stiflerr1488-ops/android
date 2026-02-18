b90f1a810f2748b65bafc6b6d4409787a5d01300
stiflerr1488-ops <stiflerr.1488@gmail.com>
2026-02-19 02:29:58 +0300

    фывыфв

--- app/src/main/kotlin/com/example/teamcompass/ui/theme/ControlSize.kt
+++
@@ -1,9 +0,0 @@
-package com.example.teamcompass.ui.theme
-
-import androidx.compose.ui.unit.dp
-
-object ControlSize {
-    val iconButton = 48.dp
-    val railButton = 56.dp
-    val pointMarker = 48.dp
-}
